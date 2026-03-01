15b2456dcd11a5c0b85d96155ad392e0d8b2a367
amalia-ullman <mali.ullman@gmail.com>
2026-03-01 12:24:23 -0800

    we're now in kansas anymore

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 async function get_timezone_data() {
     const base_url = "/timezones";
+    //const base_url = "";
     const request_url =
         `${base_url}/data.json`;
     const request = new Request(request_url);
@@ -12,7 +13,7 @@ async function get_timezone_data() {
     console.log(location_data);
 }
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([39.833333, -98.583333], 4);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
