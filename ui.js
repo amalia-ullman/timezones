433367e120728f087e6477148ea73d8ce158d65e
amalia-ullman <mali.ullman@gmail.com>
2026-04-26 12:31:16 -0700

    imported some broken functions

--- ui.js
+++ ui.js
@@ -1,3 +1,6 @@
+import { true_time } from "./math_tools.js";
+import { Coordinate } from "./math_tools.js";
+
 var map = L.map('map').setView([39.833333, -98.583333], 4)
 map.setMaxBounds(L.latLngBounds(L.latLng(23, -125.42), L.latLng(49, -62.03)));
 
@@ -6,6 +9,8 @@ map.on('click', async function (e) {
     let popup = L.popup().setLatLng(latlng).setContent("Content copied: " + latlng).openOn(map);
     navigator.clipboard.writeText(e.latlng);
     let targetTZ = await GeoTZ.find(latlng['lat'], latlng['lng']);
+
+    console.log(true_time(new Coordinate(latlng['lat'], latlng['lng']), targetTZ))
 })
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
