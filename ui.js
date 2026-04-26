1cb84918d6bac68d6e237bb8524a0739561e6319
amalia-ullman <mali.ullman@gmail.com>
2026-04-26 11:10:14 -0700

    yay!!! it gives us timezone boy did that save me a headache

--- ui.js
+++ ui.js
@@ -1,10 +1,11 @@
 var map = L.map('map').setView([39.833333, -98.583333], 4)
 map.setMaxBounds(L.latLngBounds(L.latLng(23, -125.42), L.latLng(49, -62.03)));
 
-map.on('click', function (e) {
-    var latlng = e.latlng;
-    var popup = L.popup().setLatLng(latlng).setContent("Content copied: " + latlng).openOn(map);
+map.on('click', async function (e) {
+    let latlng = e.latlng;
+    let popup = L.popup().setLatLng(latlng).setContent("Content copied: " + latlng).openOn(map);
     navigator.clipboard.writeText(e.latlng);
+    console.log(await GeoTZ.find(latlng['lat'], latlng['lng']));
 })
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
