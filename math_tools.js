e882e0c3a3334c31a139f8148cf40dabac6e3cec
amalia-ullman <mali.ullman@gmail.com>
2026-05-24 12:00:16 -0700

    no more weird json

--- math_tools.js
+++ math_tools.js
@@ -18,8 +18,6 @@ async function get_timezone_data() {
 
     const response = await fetch(request);
     const location_data = await response.json();
-    const preEl = document.querySelector('#pre');
-    preEl.textContent = JSON.stringify(location_data);
 
     console.log(location_data);
     return location_data;
