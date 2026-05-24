03bc97bfde5ab01cb8d29e84dc98ae5d78b70815
amalia-ullman <mali.ullman@gmail.com>
2026-05-24 12:27:35 -0700

    true time popups!!!

--- ui.js
+++ ui.js
@@ -7,11 +7,18 @@ map.setMaxBounds(L.latLngBounds(L.latLng(23, -125.42), L.latLng(49, -62.03)));
 map.on('click', async function (e) {
     let latlng = e.latlng;
     let targetTZ = await GeoTZ.find(latlng['lat'], latlng['lng']);
-    let true_time_result = await true_time(new Coordinate(latlng['lat'], latlng['lng']), targetTZ);
+    let true_time_result = await true_time(new Coordinate(latlng['lat'], latlng['lng']), targetTZ); // time offset but ill leave it as true_time_result
 
-    let popup = L.popup().setLatLng(latlng).setContent("additional minutes from commonly understood 'time' " + true_time_result).openOn(map);
-    // navigator.clipboard.writeText(e.latlng);
-    console.log(true_time_result);
+    const now = new Date();
+    const zone_now = new Date(now.toLocaleString('en-US', { timeZone: targetTZ[0] }));
+    const actual_true_time = new Date(zone_now.getTime() + true_time_result * 60000);
+
+    const fmt = (d) => d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+    let popup = L.popup()
+        .setLatLng(latlng)
+        .setContent(`${fmt(actual_true_time)} (${fmt(zone_now)})`)
+        .openOn(map);
 })
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
